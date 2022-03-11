17ed0040e5ffc3bfb799dbf6438fa4e570f39617
Sam Gamble <hexadessa@gmail.com>
2022-03-11 12:54:43 +0000

    Test worker wait loop

--- buildscript/utils.sh
+++ buildscript/utils.sh
@@ -187,7 +187,12 @@ run_test(){
     timeout_val=28800
     local tester=' -f compose/model.tester.yml'
     eval $(compose_model)${tester}' up -d'
-    bash -c '''$(compose_oasis)$tester logs -f --tail="all" worker | { sed "/Connected to amqp/ q" && kill -PIPE $$ ; }'  > /dev/null 2>&1
+
+    while ! $(compose_oasis)$tester logs worker | grep -q "Connected to amqp";
+    do
+        sleep 1
+        echo 'Waiting for worker'
+    done
     eval "timeout $timeout_val "$(compose_model)${tester}' run --rm --entrypoint="bash -c " model_tester "./runtest '"$@"'"'
 }
 
